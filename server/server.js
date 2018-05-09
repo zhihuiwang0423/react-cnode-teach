a8471b47fe6e0e0ff6d88ad1c5e2455aad3ab109
zhihuiwang0423 <zhihuiwang0423@gmail.com>
2018-05-09 10:03:04 +0800

    add cnode api 代理实现

--- server/server.js
+++ server/server.js
@@ -2,7 +2,23 @@ const express = require('express')
 const ReactSSR = require('react-dom/server')
 const fs = require('fs')
 const path = require('path')
+const session = require('express-session') // 获取session
+const bodyParser = require('body-parser') // 把application数据转化成req.body上数据
 const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use(session({
+  maxAge: 10 * 60 * 1000,
+  name: 'tid',
+  resave: false, // 每次请求是否重新生成id
+  saveUninitialized: false,
+  secret: 'react node class'
+}))
+
+app.use('/api/user', require('./util/handle-login'))
+app.use('./api', require('./util/proxy'))
 
 const isDev = process.env.NODE_ENV === 'development'
 if (!isDev) {
--- server/util/dev-static.js
+++ server/util/dev-static.js
@@ -3,7 +3,7 @@ const webpack = require('webpack')
 const path = require('path')
 const MemoryFs = require('memory-fs')
 const proxy = require('http-proxy-middleware')
-const ReactDomServer = require('react-dom/server')
+// const ReactDomServer = require('react-dom/server')
 const serverConfig = require('../../build/webpack.config.server')
 const axios = require('axios')
 const getTimeplate = () => {
@@ -21,7 +21,6 @@ const Module = module.constructor
 const mfs = new MemoryFs()
 const serverCompiler = webpack(serverConfig)
 serverCompiler.outputFileSystem = mfs
-let serverBundle
 serverCompiler.watch({}, (err, stats) => {
   if (err) throw err
   stats = stats.toJson()
@@ -34,7 +33,8 @@ serverCompiler.watch({}, (err, stats) => {
   const bundle = mfs.readFileSync(bundlePath, 'utf-8')
   const m = new Module()
   m._compile(bundle, 'server-entry.js')
-  serverBundle = m.exports.default
+  // let serverBundle
+  // serverBundle = m.exports.default
 })
 module.exports = function (app) {
   app.use('/public', proxy({
